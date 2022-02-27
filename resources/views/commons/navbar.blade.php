b35efa7d291e53adfacaa0020297e6673ab1ab7c
candysan777 <learning123@gmail.com>
2022-02-27 09:35:28 +0000

    create top-page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,25 @@
+<header class="mt-0">
+    <nav class="navbar navbar-expand-sm navbar-dark" style="background-color:#8E806A">
+        <div class="container-fluid">
+            {{-- トップページへのリンク --}}
+            <a class="py-4 navbar-brand" href="/">Fluffy Photo</a>
+        
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-bar">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="nav-bar">
+                <ul class="navbar-nav mr-auto"></ul>
+                <ul class="navbar-nav">
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li class="nav item">
+                        <a class="nav-link" href="#">会員登録</a>
+                    </li>
+                    {{-- ログインページへのリンク --}}
+                    <li class="nav item">
+                        <a class="nav-link" href="#">ログイン</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
